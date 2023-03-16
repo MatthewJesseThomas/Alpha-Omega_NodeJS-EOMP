3a1fb24829a3fb6b741d0e9712c5b6751b09c789
Camryn96 <herandiencamryn@gmail.com>
2023-03-16 09:25:24 +0200

    Views

--- alpha-omega/src/views/HomeView.vue
+++ alpha-omega/src/views/HomeView.vue
@@ -2,13 +2,13 @@
 	<div class="home">
 	<h1>Hello Gamers!!!</h1>
   </div>
-  <Carousel/>
+  <CarouselComponent/>
 </template>
 <script>
-import Carousel from "../components/CarouselComponent.vue"
+import CarouselComponent from "../components/CarouselComponent.vue"
  export default {
    components: {
-	Carousel
+	CarouselComponent
    }
  }
 </script>
--- alpha-omega/src/views/ProductView.vue
+++ alpha-omega/src/views/ProductView.vue
@@ -1,7 +1,8 @@
 <template>
 	<div class="ProductView">
+		<h2 class="text-dark">Welcome to the Inferno</h2>
+		<Products/>
 	</div>
-	<Products/>
 </template>
 <script>
 import Products from '../components/ProductsComp.vue'
