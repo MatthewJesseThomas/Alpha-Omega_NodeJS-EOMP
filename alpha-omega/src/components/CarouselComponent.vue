f1581baf987e01ed73f78756f65af4bf5d0d1ec4
Camryn96 <herandiencamryn@gmail.com>
2023-03-16 09:08:19 +0200

    Carousel

--- alpha-omega/src/components/CarouselComponent.vue
+++ alpha-omega/src/components/CarouselComponent.vue
@@ -3,13 +3,13 @@
 	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="..." alt="First slide">
+      <img class="d-block w-100" src="" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide">
+      <img class="d-block w-100" src="" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
+      <img class="d-block w-100" src="" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -21,14 +21,11 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
-
-
 </div>
 </template>
 <script>
 export default{
-	// eslint-disable-next-line vue/multi-word-component-names
-	name:'CarouselComponent'
+	
 };
 
 
